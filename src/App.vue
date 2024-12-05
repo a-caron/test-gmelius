5d75d3ac0358b6566250e9c1576e078d68154ea1
a-caron <antony.caron.burdiak@gmail.com>
2024-12-05 14:06:47 +0200

    Add to do

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 
+  // to do : pinia store
+  // to do : fetch data
+  // to do : display data in vuetify data-table
+  // to do : add pagination
+  // to do : add tests (cypress ?)
+  // backlog : Add a filter on the sound file length if is smaller that 30 seconds or bigger with a switch
+  // backlog : Integrate a small player in the datatable
 </script>
 
 <template>
-  Entry point
+
 </template>
 
 <style scoped>
