999d5843ceb8f9f38894919ffe4377af16761591
a-caron <antony.caron.burdiak@gmail.com>
2024-12-05 15:21:48 +0200

    Fetch birds

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 
-  // in progress : pinia store
-
   import { storeToRefs } from 'pinia'
   import { useBirdStore } from '../stores/bird.ts'
 
-  const { birds } = storeToRefs(useBirdStore())
+  const { birdList } = storeToRefs(useBirdStore())
 
-  // to do : fetch data
   // to do : display data in vuetify data-table
   // to do : add pagination
   // to do : add tests (cypress ?)
   // backlog : Add a filter on the sound file length if is smaller that 30 seconds or bigger with a switch
   // backlog : Integrate a small player in the datatable
+  // backlog : UI
 </script>
 
 <template>
-  {{ birds }}
+  <div v-for="recording in birdList">
+    {{ recording.en }}
+  </div>
 </template>
 
 <style scoped>
--- stores/bird.ts
+++ stores/bird.ts
@@ -1,11 +1,92 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
+type Response<T> = {
+  numRecordings: string
+  numSpecies: string
+  page: number
+  numPages: number
+  recordings: T[]
+}
+
+type Recording = {
+  id: string
+  gen: string
+  sp: string
+  ssp: string
+  group: string
+  en: string
+  rec: string
+  cnt: string
+  loc: string
+  lat: number | null
+  lng: number | null
+  alt: string
+  type: string
+  sex: string
+  stage: string
+  method: string
+  url: string
+  file: string
+  'file-name': string
+  sono: {
+    small: string
+    med: string
+    large: string
+    full: string
+  }
+  osci: {
+    small: string
+    med: string
+    large: string
+  },
+  lic: string
+  q: string
+  length: string
+  time: string
+  date: string
+  uploaded: string
+  also: string[]
+  rmk: string
+  'bird-seen': string
+  'animal-seen': string
+  'playback-used': string
+  temp: string
+  regnr: string
+  auto: string
+  dvc: string
+  mic: string
+  smp: string
+}
+
 export const useBirdStore = defineStore('bird', () => {
 
-  const birds = ref('hello from store')
+  const isFetching = ref(false)
+  const error = ref<string | null | unknown>(null)
+  const list = ref<Recording[] | null>(null)
+
+  fetchBirds()
+
+  async function fetchBirds() {
+
+    isFetching.value = true
+    error.value = null
+
+    try {
+      const response = await fetch('https://xeno-canto.org/api/2/recordings?query=gen:Grus+stage:juvenile+cnt:France')
+      if (!response.ok) error.value = `HTTP error, status: ${response.status}`
+      const data: Response<Recording> = await response.json()
+      list.value = data.recordings
+    } catch (e) {
+      error.value = error
+    } finally {
+      isFetching.value = false
+    }
+  }
 
   return {
-    birds
+    birdIsFetching: isFetching,
+    birdError: error,
+    birdList: list
   }
 })
